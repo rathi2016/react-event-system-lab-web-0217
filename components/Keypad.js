ef52678ba087de4997af7905334dc83dd6e227c9
github username <github email address>
2017-04-04 17:09:17 -0400

    started working

---
+++ components/Keypad.js
@@ -0,0 +1,16 @@
+import React from 'react';
+export default class Keypad extends React.Component {
+  constructor(){
+    super();
+
+    this.enterPassword = this.enterPassword.bind(this);
+  }
+  enterPassword() {
+    console.log('Entering password...')
+  }
+render(){
+  return(
+     <input type="password" onKeyUp={this.enterPassword}/>
+  );
+}
+}
